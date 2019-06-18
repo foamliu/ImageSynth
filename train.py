9f7c98c919d4cda12edb35ae53b577be1de1ba74
Yang Liu <liuyang12@focusmedia.cn>
2019-06-18 13:37:46 +0800

    update

--- train.py
+++ train.py
@@ -1,10 +1,17 @@
 from fastai.vision import *
+from fastai.callbacks.hooks import *
 
-path = 'data/captures'
+if __name__ == "__main__":
+    path = 'data/captures'
 
-fnames = list((path / 'train').glob('*img*'))
-lbl_names = list((path / 'train').glob('*layer*'))
+    fnames = list((path / 'train').glob('*img*'))
+    print(fnames[:3])
+    lbl_names = list((path / 'train').glob('*layer*'))
+    print(lbl_names[:3])
 
-img_f = fnames[0]
-img = open_image(img_f)
-img.show(figsize=(5, 5))
+    img_f = fnames[0]
+    img = open_image(img_f)
+    img.show(figsize=(5, 5))
+
+
+
